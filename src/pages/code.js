65977d97d043989b3042b345234b130fb0e37b55
Glitch (moto-server) <none>
2022-01-27 15:46:26 +0000

    🚟🕶 Checkpoint ./src/pages/code.js:41413781/736 ./src/pages/servicio.html:41413781/144

---
+++ src/pages/code.js
@@ -0,0 +1,21 @@
+function setCookie(cname, cvalue, exhor) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exhor * 3600 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
