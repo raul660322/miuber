2c27ad667fe0098092050950733c4a970a406ff9
Glitch (moto-server) <none>
2022-01-27 21:57:50 +0000

    💡🙅 Checkpoint ./public/data.js:41413781/563

--- public/data.js
+++ public/data.js
@@ -1,5 +1,10 @@
 var zonas = [
-  
-
+  {"nombre":"Pre Universitario","lat":1,"long":1},
+  {"nombre":"Parque de la Libertad","lat":1,"long":1},
+  {"nombre":"Plaza","lat":1,"long":1},
+  {"nombre":"Teatro Sauto","lat":1,"long":1},
+  {"nombre":"Hospital Pediátrico","lat":1,"long":1},
+  {"nombre":"Hospital Faustino","lat":1,"long":1},
+  {"nombre":"Ho","lat":1,"long":1}
   
 ]
