9a9b8c0eb156d3e466de4bc938267cd1f15ac9a1
Glitch (moto-server) <none>
2022-02-12 23:00:29 +0000

    🕰🐞 Checkpoint ./src/pages/servicio.html:41413781/332 ./server.js:41413781/4

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ io.on('connection', (socket) => {
            const fecha = await fastify.level.db.get(telefono);
            const fechaActual = new Date().getTime();
            if (fecha && (fecha + MES > fechaActual)){
-             callback(null,{'telefono':telefono, 'fecha':fecha});
+             callback(null,{'telefono':telefono, 'fecha':fecha+MES});
            } else {
              callback(null,{'telefono':telefono, 'fecha':0});
            } 
